438ad3b48254c659f0d32452834313b15b631372
PVGlimoe <phillip.glimoe@hotmail.com>
2020-10-04 18:40:13 +0200

    .

--- bankOpgave/src/main/java/com/example/demo/controllers/MyController.java
+++ bankOpgave/src/main/java/com/example/demo/controllers/MyController.java
@@ -1,7 +1,12 @@
 package com.example.demo.controllers;
 
+import com.example.demo.models.Customer;
+import org.apache.catalina.filters.ExpiresFilter;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import javax.servlet.http.HttpServletRequest;
 
 @Controller
 public class MyController {
@@ -19,5 +24,13 @@ public class MyController {
     @GetMapping("/login")
     public String login(){
         return "login";
+    }
+
+    @PostMapping("/postAccount")
+    public String postAccount(HttpServletRequest request){
+        String firstName = request.getParameter("firstName");
+        String lastName = request.getParameter("lastName");
+        Customer customer = new Customer(firstName, lastName);
+        return "redirect:/";
     }
 }
--- bankOpgave/src/main/java/com/example/demo/models/Customer.java
+++ bankOpgave/src/main/java/com/example/demo/models/Customer.java
@@ -3,11 +3,13 @@ package com.example.demo.models;
 public class Customer {
     private String firstName;
     private String lastName;
-    static int customerId = 1;
+    private int id;
+    static int customerId = 0;
 
     public Customer(String firstName, String lastName){
         this.firstName = firstName;
         this.lastName = lastName;
         customerId++;
+        id = customerId;
     }
 }
